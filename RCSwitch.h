39e8fae226563121d4e394f9d5b31156653edb2f
Robert ter Vehn <robert@tervehn.se>
2016-04-28 22:32:22 +0200

    Removed unnecessary 'extern C'-block

--- RCSwitch.h
+++ RCSwitch.h
@@ -49,14 +49,8 @@
     #define CHANGE 1
     // The following typedefs are needed to be able to compile RCSwitch.cpp
     // with the RPi C++ compiler (g++)
-    #ifdef __cplusplus
-        extern "C"{
-    #endif
-        typedef uint8_t boolean;
-        typedef uint8_t byte;
-    #ifdef __cplusplus
-    }
-    #endif
+    typedef uint8_t boolean;
+    typedef uint8_t byte;
     // Last line within Raspberry Pi block
 #else
     #include "WProgram.h"
