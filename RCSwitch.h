6500e3ae75e2d3fbc42549b9a154229923396683
Sebastian Herber <herber.sebastian@gmail.com>
2016-05-11 10:29:59 +0200

    fixed intendation

--- RCSwitch.h
+++ RCSwitch.h
@@ -123,7 +123,7 @@ class RCSwitch {
     char* getCodeWordB(int nGroupNumber, int nSwitchNumber, bool bStatus);
     char* getCodeWordC(char sFamily, int nGroup, int nDevice, bool bStatus);
     char* getCodeWordD(char group, int nDevice, bool bStatus);
-	char* getCodeWordE(int sGroup, int nDevice, bool bStatus);
+    char* getCodeWordE(int sGroup, int nDevice, bool bStatus);
 
     void transmit(HighLow pulses);
 
