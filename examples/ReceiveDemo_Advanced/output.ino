4aa1cd563d87926cff438d42cc9ea07ab251f489
Suat Özgür <s.oezguer@gmail.com>
2016-05-26 10:54:14 +0200

    Revert "changed function order to make travis compile, hopefully..."
    This reverts commit 067ab194ca71ef633e9bd37427e28eed640c69d4.

--- examples/ReceiveDemo_Advanced/output.ino
+++ examples/ReceiveDemo_Advanced/output.ino
@@ -1,3 +1,33 @@
+void output(unsigned long decimal, unsigned int length, unsigned int delay, unsigned int* raw, unsigned int protocol) {
+
+  if (decimal == 0) {
+    Serial.print("Unknown encoding.");
+  } else {
+    const char* b = dec2binWzerofill(decimal, length);
+    Serial.print("Decimal: ");
+    Serial.print(decimal);
+    Serial.print(" (");
+    Serial.print( length );
+    Serial.print("Bit) Binary: ");
+    Serial.print( b );
+    Serial.print(" Tri-State: ");
+    Serial.print( bin2tristate( b) );
+    Serial.print(" PulseLength: ");
+    Serial.print(delay);
+    Serial.print(" microseconds");
+    Serial.print(" Protocol: ");
+    Serial.println(protocol);
+  }
+  
+  Serial.print("Raw data: ");
+  for (unsigned int i=0; i<= length*2; i++) {
+    Serial.print(raw[i]);
+    Serial.print(",");
+  }
+  Serial.println();
+  Serial.println();
+}
+
 static const char* bin2tristate(const char* bin) {
   static char returnValue[50];
   int pos = 0;
@@ -39,34 +69,3 @@ static char * dec2binWzerofill(unsigned long Dec, unsigned int bitLength) {
   
   return bin;
 }
-
-void output(unsigned long decimal, unsigned int length, unsigned int delay, unsigned int* raw, unsigned int protocol) {
-
-  if (decimal == 0) {
-    Serial.print("Unknown encoding.");
-  } else {
-    const char* b = dec2binWzerofill(decimal, length);
-    Serial.print("Decimal: ");
-    Serial.print(decimal);
-    Serial.print(" (");
-    Serial.print( length );
-    Serial.print("Bit) Binary: ");
-    Serial.print( b );
-    Serial.print(" Tri-State: ");
-    Serial.print( bin2tristate( b) );
-    Serial.print(" PulseLength: ");
-    Serial.print(delay);
-    Serial.print(" microseconds");
-    Serial.print(" Protocol: ");
-    Serial.println(protocol);
-  }
-  
-  Serial.print("Raw data: ");
-  for (unsigned int i=0; i<= length*2; i++) {
-    Serial.print(raw[i]);
-    Serial.print(",");
-  }
-  Serial.println();
-  Serial.println();
-}
-
