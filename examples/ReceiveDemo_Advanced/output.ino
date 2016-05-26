493f436b0bd6d8e79b1419d245b35471ebcaa681
bau-sec <bauer.rhit@gmail.com>
2016-05-25 20:37:24 -0400

    Fix Travis CI build

--- examples/ReceiveDemo_Advanced/output.ino
+++ examples/ReceiveDemo_Advanced/output.ino
@@ -1,3 +1,6 @@
+static const char* bin2tristate(const char* bin);
+static char * dec2binWzerofill(unsigned long Dec, unsigned int bitLength);
+
 void output(unsigned long decimal, unsigned int length, unsigned int delay, unsigned int* raw, unsigned int protocol) {
 
   if (decimal == 0) {
