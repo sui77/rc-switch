7d934e591c218fc979a06e8fc0d1a53dc11aa237
quaec <quaec.0@gmail.com>
2017-02-10 17:44:02 +0100

    Changed order of setProtocol and setPulseLength
    Changed the order of "mySwitch.setProtocol(2);" and "mySwitch.setPulseLength(320);", so setting the PulseLength will work when both lines are commented out. See issue #117

--- examples/SendDemo/SendDemo.ino
+++ examples/SendDemo/SendDemo.ino
@@ -15,12 +15,12 @@ void setup() {
   
   // Transmitter is connected to Arduino Pin #10  
   mySwitch.enableTransmit(10);
-
-  // Optional set pulse length.
-  // mySwitch.setPulseLength(320);
   
   // Optional set protocol (default is 1, will work for most outlets)
   // mySwitch.setProtocol(2);
+
+  // Optional set pulse length.
+  // mySwitch.setPulseLength(320);
   
   // Optional set number of transmission repetitions.
   // mySwitch.setRepeatTransmit(15);
