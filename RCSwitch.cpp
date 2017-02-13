c2aea10ec33f8f57775c8c34951b2cc702f29593
krzmazur1 <mazirius@gmail.com>
2017-02-13 17:18:57 +0100

    Update RCSwitch.cpp

--- RCSwitch.cpp
+++ RCSwitch.cpp
@@ -78,7 +78,7 @@ static const RCSwitch::Protocol PROGMEM proto[] = {
   { 100, { 30, 71 }, {  4, 11 }, {  9,  6 }, false },    // protocol 3
   { 380, {  1,  6 }, {  1,  3 }, {  3,  1 }, false },    // protocol 4
   { 500, {  6, 14 }, {  1,  2 }, {  2,  1 }, false },    // protocol 5
-  { 450, { 23,  1 }, {  1,  2 }, {  2,  1 }, true }      // protocol 6 (HT6P20B)
+  { 450, { 23,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 6 (HT6P20B)
   { 320, { 36,  1 }, {  1,  2 }, {  2,  1 }, true }      // protocol 7 (SM5212)
 };
 
