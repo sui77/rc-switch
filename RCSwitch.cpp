3d15bf0025fdfa8d160fae98e5382e96ac8e2610
g3rb3n <gerbenvaneerten@gmail.com>
2017-06-01 01:31:06 +0200

    Added protocol 7 for InterTechno PAR-1000 remote

--- RCSwitch.cpp
+++ RCSwitch.cpp
@@ -13,6 +13,7 @@
   - Robert ter Vehn / <first name>.<last name>(at)gmail(dot)com
   - Johann Richard / <first name>.<last name>(at)gmail(dot)com
   - Vlad Gheorghe / <first name>.<last name>(at)gmail(dot)com https://github.com/vgheo
+  - Gerben van Eerten / gerbenvaneerten(at)gmail(dot)com
   
   Project home: https://github.com/sui77/rc-switch/
 
@@ -78,7 +79,8 @@ static const RCSwitch::Protocol PROGMEM proto[] = {
   { 100, { 30, 71 }, {  4, 11 }, {  9,  6 }, false },    // protocol 3
   { 380, {  1,  6 }, {  1,  3 }, {  3,  1 }, false },    // protocol 4
   { 500, {  6, 14 }, {  1,  2 }, {  2,  1 }, false },    // protocol 5
-  { 450, { 23,  1 }, {  1,  2 }, {  2,  1 }, true }      // protocol 6 (HT6P20B)
+  { 450, { 23,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 6 (HT6P20B)
+  { 500, {  1, 23 }, {  1,  2 }, {  2,  1 }, false }     // protocol 7
 };
 
 enum {
