372013d7ce9b915bf03c81030176aee37ca00d22
Dominik Palo <dominik@palo.sk>
2017-11-07 00:11:04 +0100

    Remove unnecessary conditions from examples

--- examples/ReceiveDemo_Simple/ReceiveDemo_Simple.ino
+++ examples/ReceiveDemo_Simple/ReceiveDemo_Simple.ino
@@ -16,19 +16,13 @@ void setup() {
 void loop() {
   if (mySwitch.available()) {
     
-    int value = mySwitch.getReceivedValue();
-    
-    if (value == 0) {
-      Serial.print("Unknown encoding");
-    } else {
-      Serial.print("Received ");
-      Serial.print( mySwitch.getReceivedValue() );
-      Serial.print(" / ");
-      Serial.print( mySwitch.getReceivedBitlength() );
-      Serial.print("bit ");
-      Serial.print("Protocol: ");
-      Serial.println( mySwitch.getReceivedProtocol() );
-    }
+    Serial.print("Received ");
+    Serial.print( mySwitch.getReceivedValue() );
+    Serial.print(" / ");
+    Serial.print( mySwitch.getReceivedBitlength() );
+    Serial.print("bit ");
+    Serial.print("Protocol: ");
+    Serial.println( mySwitch.getReceivedProtocol() );
 
     mySwitch.resetAvailable();
   }
