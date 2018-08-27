0e6d16e3cc9d7e588131bb5df54ad4e4dd399105
Robert ter Vehn <robert@tervehn.se>
2018-08-28 01:29:35 +0200

    Removed debug print

--- RCSwitch.cpp
+++ RCSwitch.cpp
@@ -30,7 +30,6 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-#include <stdio.h>
 #include "RCSwitch.h"
 
 #ifdef RaspberryPi
@@ -486,7 +485,6 @@ char* RCSwitch::getCodeWordE(const char* sGroup, const char* sDevice, bool bStat
   sReturn[nReturnPos++] = bStatus ? '0' : 'F';
 
   sReturn[nReturnPos] = '\0';
-  printf("tristate code word: %s\n", sReturn);
   return sReturn;
 }
 
