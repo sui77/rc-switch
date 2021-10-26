469ede1c9cdd4b5a33019665161b054a46371dd5
LaughingLeeLife <45506527+LaughingLeeLife@users.noreply.github.com>
2021-10-26 11:01:51 +0800

    Update RCSwitch.cpp
    Add one protocol for Dooya Cover Motor remote control . 
    Dooya Cover Motor is widely used in the world.

--- RCSwitch.cpp
+++ RCSwitch.cpp
@@ -90,7 +90,8 @@ static const RCSwitch::Protocol PROGMEM proto[] = {
   { 200, { 130, 7 }, {  16, 7 }, { 16,  3 }, true},      // protocol 9 Conrad RS-200 TX
   { 365, { 18,  1 }, {  3,  1 }, {  1,  3 }, true },     // protocol 10 (1ByOne Doorbell)
   { 270, { 36,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 11 (HT12E)
-  { 320, { 36,  1 }, {  1,  2 }, {  2,  1 }, true }      // protocol 12 (SM5212)
+  { 320, { 36,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 12 (SM5212)
+  { 350, { 14,  4 }, {  1,  2 }, {  2,  1 }, false }     // protocol 13 (Dooya Cover)
 };
 
 enum {
