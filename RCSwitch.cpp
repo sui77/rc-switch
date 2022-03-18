1c94f83f1140650d6b11099643b94688df258603
youri007 <yhardoo77@gmail.com>
2022-03-18 20:41:02 +0100

    Update RCSwitch.cpp

--- RCSwitch.cpp
+++ RCSwitch.cpp
@@ -90,7 +90,10 @@ static const RCSwitch::Protocol PROGMEM proto[] = {
   { 200, { 130, 7 }, {  16, 7 }, { 16,  3 }, true},      // protocol 9 Conrad RS-200 TX
   { 365, { 18,  1 }, {  3,  1 }, {  1,  3 }, true },     // protocol 10 (1ByOne Doorbell)
   { 270, { 36,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 11 (HT12E)
-  { 320, { 36,  1 }, {  1,  2 }, {  2,  1 }, true }      // protocol 12 (SM5212)
+  { 320, { 36,  1 }, {  1,  2 }, {  2,  1 }, true } // protocol 12 (SM5212)
+  { 415, { 1, 30 }, { 1, 3 }, { 4, 1 }, false }      // protocol 13 (sc2260R4)
+};
+  // protocol 12 (SM5212)
 };
 
 enum {
